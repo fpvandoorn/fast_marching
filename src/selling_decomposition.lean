2c3ee89b8364599b4af9627f0548515962a25aa3
angele-lochet <129400884+angele-lochet@users.noreply.github.com>
2023-04-28 10:11:48 +0200

    lemma about norm D

--- src/selling_decomposition.lean
+++ src/selling_decomposition.lean
@@ -1,6 +1,7 @@
 import linear_algebra.matrix.pos_def
 import data.complex.basic
 import preliminaries
+open nat
 
 /--
 Following appendix B of the paper
@@ -124,6 +125,54 @@ lemma exercise_part_one (e : matrix (fin 3) (fin 2) R) (he : is_superbase e) :
 
   end
 
+lemma exercise_lemma (e : matrix (fin 4) (fin 3) R) (he : is_superbase e) :
+  univ.sum e = e 0 + (e 1 + (e 2 + e 3)) :=
+  begin
+    rw fin.sum_univ_def,simp[sum_range_succ],
+    have m : list.fin_range 4 = [0,1,2,3],
+    refl, rw m,simp,
+  end
+
+
+lemma exercise_bis (e : matrix (fin 4) (fin 3) R) (he : is_superbase e) :
+  is_superbase ![- e 0, e 1, e 2 + e 0, e 3 + e 0] :=
+
+  begin
+    simp only [is_superbase],
+    split,
+    {simp,
+    ring,
+    have h : univ.sum e = e 0 + (e 1 + (e 2 + e 3)),
+    apply exercise_lemma,
+    apply he,
+    rw ← h,
+    apply he.1,
+    },
+    rw det_fin_three,
+    have g : e 1 = - e 0 - e 2 - e 3,
+    {have k:= exercise_lemma e he,
+    have h:=he.1,
+    rw k at h,
+    rw [← sub_zero (e 0),←h ],
+    ring,
+    },
+    rw g,
+    simp,
+    unfold is_superbase at he,
+    rw det_fin_three at he,
+    simp at he,
+    have h:=he.2,
+    rw ← he.2,
+    --rw e 2.succ 0 at h,
+    --rw [nat.succ 2, nat.succ 1, nat.succ 0],
+    rw abs_eq_abs,
+
+
+
+  end
+
+#check [0,1,1].succ 0
+
 /-- def B.1 -/
 def is_obtuse (v : matrix (fin (d+1)) (fin d) R) (D : matrix (fin d) (fin d) R) : Prop :=
 ∀ i j : fin (d+1), i < j → v i ⬝ᵥ D.mul_vec (v j) ≤ 0
@@ -135,6 +184,73 @@ section Z_or_R
 variables {v : matrix (fin (d+1)) (fin d) R} {D : matrix (fin d) (fin d) R}
 
 example {D : matrix (fin d) (fin d) ℝ} (Dposdef : D.pos_def) : 0 < D.det := Dposdef.det_pos
+
+/-- The definition of the norm D ; careful, vector taken not superbase-/
+
+def norm_D (v : matrix (fin d) unit R) (D : matrix (fin d) (fin d) R) := (transpose (v)).mul(D.mul v)
+
+
+variables {A : matrix (fin d) (fin d) R} {B : matrix (fin d) (fin d) R}
+#check(A.mul B)
+
+
+def E_D (v : matrix (fin (d+1)) (fin d) R) (D : matrix (fin d) (fin d) R) := ∑ i , norm_D (matrix.col (v i)) (D)
+
+lemma mul_add_col (v : matrix (fin 3) (fin 2) R) (D : matrix (fin d) (fin 2) R) (i : nat)(j : nat): 
+  D ⬝ (col (v i) + col (v j)) = D.mul(col (v i)) + D.mul(col (v j)) :=
+  begin
+    apply matrix.mul_add,
+  end
+
+lemma add_mul_row (v : matrix (fin 3) (fin 2) R) (D : matrix (fin 2) (fin d) R) (i : nat)(j : nat): 
+  (row (v i) + row (v j)).mul D = (row (v i)).mul D + (row(v j)).mul D :=
+  begin
+    apply matrix.add_mul,
+  end
+
+
+
+
+
+lemma E_D_superbase (v : matrix (fin 3) (fin 2) R) (D : matrix (fin 2) (fin 2) R) (he : is_superbase v) :
+  E_D (![- v 0, v 1, v 0 - v 1]) (D) = E_D(v) (D) - 4 * (transpose (matrix.col (v 0))).mul(D.mul (matrix.col (v 1))) :=
+  begin
+    unfold E_D,
+    ring_nf,
+    unfold norm_D,
+    rw exercise_part_one (v) (he),
+    --have g := (col (![-(-v 1 - v 2), v 1, -v 1 - v 2 - v 1] x))ᵀ ⬝ (D ⬝ col (![-(-v 1 - v 2), v 1, -v 1 - v 2 - v 1] x)),
+    have h2 : univ.sum v = v 0 + v 1 + v 2,
+    {rw fin.sum_univ_def,simp[sum_range_succ],
+    have m: list.fin_range 3 = [0,1,2],
+    refl,rw m,simp,rw add_assoc,},
+    rw fin.sum_univ_def,
+    rw fin.sum_univ_def,
+    simp[sum_range_succ],
+    have m: list.fin_range 3 = [0,1,2],
+    refl,
+    rw m,
+    simp,
+    ring_nf,
+    have m1 : D ⬝ (col (v 2) + col (v 1)) = D.mul(col (v 2)) + D.mul(col (v 1)),
+    {apply mul_add_col,},
+    rw m1,
+    have H := (D ⬝ col (v 2) + D ⬝ col (v 1)),
+    have m2 : (row (v 2) + row (v 1)).mul((D ⬝ col (v 2) + D ⬝ col (v 1)))= (row (v 2)).mul((D ⬝ col (v 2) + D ⬝ col (v 1))) + (row (v 1)).mul((D ⬝ col (v 2) + D ⬝ col (v 1))),
+    {apply add_mul_row,},
+    rw m2,
+    apply mul_add_col,
+    have m3 : row (v 2) ⬝ (D ⬝ col (v 2) + D ⬝ col (v 1)) + row (v 1) ⬝ (D ⬝ col (v 2) + D ⬝ col (v 1)) + (row (v 1) ⬝ (D ⬝ col (v 1)) + row (-(2 * v 1) - v 2) ⬝ (D ⬝ col (-(2 * v 1) - v 2))) = row (v 2).mul(D ⬝ col (v 2)) + row (v 2).mul(D ⬝ col (v 1)) + row (v 1) ⬝ (D ⬝ col (v 2)) + row (v 1) ⬝ D ⬝ col (v 1) + (row (v 1) ⬝ (D ⬝ col (v 1)) + row (-(2 * v 1) - v 2) ⬝ (D ⬝ col (-(2 * v 1) - v 2))),
+    {repeat {apply mul_add_col,},}
+
+
+    
+
+
+
+  end 
+
+
 
 /-- The definition of the `e_{i,j}` -/
 def associated_vectors (v : matrix (fin (d+1)) (fin d) R) (i j : fin (d+1)) (l : fin d) : R :=
